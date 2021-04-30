28d5c64a0d91e73014b14a2d355cadb7923458f6
Brian Ataria Ceraldi <brianatariaceraldi@gmail.com>
2021-04-30 20:38:57 -0300

    adding the painting event

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ let cell = [];
 const container = document.querySelector("#container");
 container.style.gridTemplateColumns = ("repeat(16), 1fr");
 container.style.gridTemplateRows = ("repeat(16), 1fr");
+
 let numberOfCells = 16 * 16
 
-    for(let i = 0; i < numberOfCells; i++) {
-        cell[i] = document.createElement("div");
-        cell[i].classList.add('cells');
-        container.appendChild(cell[i]);
-    }
-    
-;
+for(let i = 0; i < numberOfCells; i++) {
+    cell[i] = document.createElement("div");
+    cell[i].classList.add('cells');
+    container.appendChild(cell[i]);
+}
+
+const cells = document.querySelectorAll(".cells");
+
+cells.forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = 'black'
+    })
+})
+
+
