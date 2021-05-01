e727b8ef80df4e9cc7ac757dd26711141bd08b96
Brian Ataria Ceraldi <brianatariaceraldi@gmail.com>
2021-05-01 11:22:55 -0300

    button changed

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ container.style.gridTemplateRows = ("repeat(16), 1fr");
 
 let numberOfCells = 16 * 16
 
+
 for(let i = 0; i < numberOfCells; i++) {
     cell[i] = document.createElement("div");
     cell[i].classList.add('cells');
